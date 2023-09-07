c64abdd1944cf363c4a16138292a8453fb6e0675
Larsisko <arkadiusz.miazga1988@gmail.com>
2023-09-07 23:11:37 +0200

    module7 both task done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+const galleryImageList = galleryItems
+  .map((image) => {
+    return `<li class="gallery__item">
+      <a class="gallery__link" href="${image.original}">
+        <img class="gallery__image" src="${image.preview}" alt=""${image.description}"" title="${image.description}" data-source="${image.original}" />
+      </a>
+    </li>`;
+  })
+  .join("")
+  .replace(/"/g, "");
+galleryList.insertAdjacentHTML("beforeend", galleryImageList);
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionPosition: "outside",
+  captionDelay: 250,
+  nav: false,
+  close: false,
+  showCounter: false,
+  captions: true,
+  captionDelay: 250,
+  captionData: "alt",
+  disableScroll: true,
+  scrollZoom: false,
+});
